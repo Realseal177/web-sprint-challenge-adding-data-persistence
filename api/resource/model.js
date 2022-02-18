35c1848a519aa02bfc8819fa19b48f79b04cc5f8
Drew Nelson <realseal177@gmail.com>
2022-02-18 09:12:34 -0800

    resources get call working

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,6 +1,11 @@
 // build your `Resource` model here
 const db = require('../../data/dbConfig.js');
 
+async function findAll() {
+    const resources = await db('resources');
+    return resources;
+  }
+
 module.exports = {
-    
+    findAll,
 }
--- api/resource/router.js
+++ api/resource/router.js
@@ -4,4 +4,12 @@ const router = express.Router();
 
 const Resources = require('./model')
 
+router.get('/', (req, res, next) => {
+    Resources.findAll()
+        .then(resource => {
+            res.json(resource)
+        })
+        .catch(next)
+})
+
 module.exports = router;
